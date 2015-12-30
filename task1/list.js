8bdb379b70f60ee86e537dede601d34c19a1a84a
kluf <vova.kluf@gmail.com>
2015-12-30 12:09:04 +0200

    probably module was fixed

--- task1/list.js
+++ task1/list.js
@@ -1,42 +1,51 @@
 var ListManager =(function() {
-    var addTaskEl = document.querySelector('#addTask');
-    var errorDivEl = document.querySelector('#error');
-    var mainDivEl = document.getElementById('main');
+    var mainDivEl, addTaskEl, errorDivEl, addTaskButton;
+    var CLOSE_SELECTOR_EL = '.close';
+    var VISIBLE_CLASS_SELECTOR = 'visible';
+    var TASK_CLASS_SELECTOR = 'task';
+    var CLOSE_CLASS_SELECTOR = 'close';
+    var ERROR_CLASS_SELECTOR = 'error';
+
+    function setElements(rootElSelector) {
+        mainDivEl = rootElSelector;
+        addTaskEl = mainDivEl.querySelector('.addTask');
+        errorDivEl = mainDivEl.querySelector('.error');
+        addTaskButton = mainDivEl.querySelector('.addTaskButton');
+    }
     function fieldContainTheText(){
         return addTaskEl.value;
     };
-    function addStyledTaskDiv(event) {
-        event.preventDefault();
+    function addStyledTaskDiv(e) {
+        setElements(e.target.parentNode);
         if (fieldContainTheText()) {
             var divElement = document.createElement('div');
-            divElement.innerHTML = "<div class='task'>"+addTaskEl.value+"<span class='close'>x</span></div>";
+            divElement.innerHTML = "<div class='"+TASK_CLASS_SELECTOR+"'>"+addTaskEl.value+"<span class='"+CLOSE_CLASS_SELECTOR+"'>x</span></div>";
+            console.log(mainDivEl);
             mainDivEl.appendChild(divElement);
-            divElement.querySelector('.close').addEventListener('click', removeTask, true);
-            toggleClass('visible');
+            divElement.querySelector(CLOSE_SELECTOR_EL).addEventListener('click', removeTask);
+            toggleClass(VISIBLE_CLASS_SELECTOR);
             resetInput();
             setFocus();
         } else {
-            toggleClass('visible');
+            toggleClass(VISIBLE_CLASS_SELECTOR);
         }
     };
     function removeTask(event) {
-        event.preventDefault();
         var nodeToRemove = event.target.parentNode;
         event.target.parentNode.parentNode.removeChild(nodeToRemove);
     };
-    function resetInput() {
+    function resetInput(e) {
         addTaskEl.value = '';
     };
     function setFocus() {
         addTaskEl.focus();
     };
     function toggleClass(className) {
-        errorDivEl.className = fieldContainTheText() ? '' : className;
+        errorDivEl.className = fieldContainTheText() ? ERROR_CLASS_SELECTOR : ERROR_CLASS_SELECTOR + ' ' + className;
     };
     return {
-        initialize: function() {
-            document.getElementById('addTaskButton').addEventListener('click', addStyledTaskDiv);
+        initialize: function(rootElSelector) {
+            rootElSelector.querySelector('.addTaskButton').addEventListener('click', addStyledTaskDiv);
         }
     }
 }());
-ListManager.initialize();
